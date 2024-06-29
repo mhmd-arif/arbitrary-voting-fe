bbe4059271e0f0604f922aadf7fdd5f5140c37cd
mhmd-arif <mhmdarif.developer@gmail>
2024-06-29 18:32:30 +0700

    feat: landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import Image from "next/image";
+"use client";
+import NavButton from "@/components/NavButton";
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <p>helo</p>
-      </div>
+      <div className="title ">Nama Penelitian</div>
+
+      <p className="description">
+        DESKRIPSI PENELITIAN <br /> Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. In vehicula nisl in vehicula tincidunt. Donec
+        vestibulum purus at vulputate faucibus. Vivamus malesuada justo aliquam
+        libero tempus, id pharetra leo tristique. Quisque convallis tortor ac
+        maximus placerat. Sed tincidunt sollicitudin augue rhoncus aliquet. Nunc
+        facilisis vitae eros eu venenatis. Cras lacinia ornare arcu, nec
+        convallis felis fermentum condimentum. Proin placerat vitae turpis ut
+        semper. Fusce nec nisl lectus. Donec efficitur iaculis ligula. Donec in
+        lectus finibus, iaculis lorem sollicitudin, feugiat tortor. Mauris
+        mauris urna, pellentesque id velit ut, aliquam ultrices lorem.
+      </p>
+
+      <NavButton href={"/simulation"} text={"Selanjutnya"} />
     </main>
   );
 }
